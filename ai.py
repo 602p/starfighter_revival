d40f40979f25539928719ffaff5ee8d78d715dbd
Louis Goessling <louis@goessling.com>
2017-05-20 19:30:11 -0500

    AAA

--- ai.py
+++ ai.py
@@ -1,3 +1,4 @@
+import pygame
 
 ais={}
 
@@ -13,8 +14,21 @@ class AI(object):
 	def update(self, dt):
 		pass
 
-@register_ai("go_forwards")
+@register_ai("projectile_ai")
 class GoForwardsAI(AI):
+	def __init__(self):
+		self.create_time=pygame.time.get_ticks()
 	def update(self, dt):
 		self.ship.accel_direction=1
 		self.ship.turn_direction=0
+		if pygame.time.get_ticks()-self.create_time>self.ship.type.raw.get("lifetime", 5000):
+			self.ship.marked_for_death=True
+
+		for ship in game.client.owned_entities.values():
+			if ship.faction!=self.ship.faction and ship is not self.ship:
+				# print(ship.rect, self.ship.rect)
+				if ship.rect.colliderect(self.ship.rect):
+					self.ship.marked_for_death=True
+					ship.take_damage(self.ship.type.raw.get("damage", 10))
+
+import game
